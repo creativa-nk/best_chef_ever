dfd83ed25875302a06da9cacc174bde08f5f0eb5
Nadia <nadiyaklokova@gmail.com>
2021-06-09 13:43:50 +0200

    mi funcion STOP funciona!

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 /*--------- Funcion para pausal el slider-------- */
 
-var imgs = $('.sliding-background'),
-playState = 'animation-play-state';
-    imgs.click(function() {
-        imgs.css(playState, function(i, v) {
-        return v === 'paused' ? 'running' : 'paused'; 
-    });
-    $('body').toggleClass('paused', $(this).css(playState) === 'paused'); });
-
+    function stop(){
+        var img = document.getElementById('sliding-background')
+        
+        if(img.style.animationPlayState === 'running'){
+            img.style.animationPlayState = 'paused'; 
+        } else {
+            img.style.animationPlayState ='running';
+        }
+    }
