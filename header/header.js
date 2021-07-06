3f0e9221c83f45446f068667655d309afba90617
Nadia <nadiyaklokova@gmail.com>
2021-07-06 15:35:36 +0200

    enlazado header

---
+++ header/header.js
@@ -0,0 +1,11 @@
+/* -----------NavBar Movil--------------- */
+menu = $("nav ul");
+toggle = $("nav ul .menu");
+
+toggle.on("click", function() {
+  if (menu.hasClass("active")) {
+    menu.removeClass("active");
+  } else {
+    menu.addClass("active");
+  }
+});
