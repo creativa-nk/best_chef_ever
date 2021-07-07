6efec0d4fd1f9fd4d4b725ffcad4c88dde664754
Nadia <nadiyaklokova@gmail.com>
2021-07-08 00:09:17 +0200

    flecha de scroll

---
+++ loader/loader.js
@@ -0,0 +1,13 @@
+/* ------------------LOADING------------------- */
+/* window.addEventListener('load',function(){
+    document.getElementById('loader').classList.toggle('loader2'),10000
+}) */
+var fn =  function(){
+    var loader = document.getElementById('loader');
+
+    loader.style.visibility = 'hidden';
+    loader.style.opacity = '0';
+    loader.style.zIndex = '-5'
+}
+
+window.onload = function() { setInterval(fn,4000)};  //4seg
