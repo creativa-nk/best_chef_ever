8d22d409cc67b207b6442d7a5beb64355dd0fe1a
Nadia <nadiyaklokova@gmail.com>
2021-07-08 23:37:50 +0200

    icono scroll experience

--- main.js
+++ main.js
@@ -37,8 +37,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-     document.getElementById("header-title-scroll-down").style.display = "none"; 
+     document.getElementById("scroll-ups").style.display = "none"; 
   } else {
-    document.getElementById("header-title-scroll-down").style.display = "block";
+    document.getElementById("scroll-ups").style.display = "block";
   }
 }
